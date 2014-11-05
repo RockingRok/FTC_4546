dfbcb3520286d753db6868585c2852d4b8d58a55
Mikalai Shlapkou <nickshlapkov@gmail.com>
2014-11-04 18:04:03 -0600

    Some minor updates
    Currently still testing the code but I made some theoretical fixes for
    some of our problems

--- library.h
+++ library.h
@@ -2,25 +2,61 @@
 #include "hitechnic-gyro.h";
 #include "hitechnic-irseeker-v2.h"
 
+//gets encoder average of the left and right motors
 int getEncoderAverage(int leftMotor, int rightMotor)
 {
 	return (leftMotor + rightMotor) / 2;
 }
 
+//gets the current IR value
 int getIR()
 {
 	return HTIRS2readACDir(irSensor);
 }
 
+//threshhold method for going forward/backward
+//essentially, if the robot is not +1/-1 degrees of going exactly forward it will return false
 bool gyroThreshhold(float heading, int trueValue, int threshhold)
 {
-	if(abs(heading) > (trueValue + threshhold))
+	if(heading > 0)
 	{
-		return false;
+		if((heading > (trueValue + threshhold)) || (heading > (trueValue - threshhold)))
+		{
+			return false;
+		}
+	}
+	if(heading < 0)
+	{
+		if((heading < (trueValue + threshhold)) || (heading < (trueValue - threshhold)))
+		{
+			return false;
+		}
 	}
 	return true;
 }
 
+//method for rotational threshhold
+bool gyroThreshRot(float heading, int trueValue, int threshhold)
+{
+	if(trueValue < 0)
+	{
+		if(heading > trueValue)
+		{
+			return false;
+		}
+	}
+	if(trueValue > 0)
+	{
+		if(heading < trueValue)
+		{
+			return false;
+		}
+	}
+	return true;
+
+}
+
+//gets our "position" on the field through our IR sensors
 int getPosition()
 {
 	int x = getIR();
@@ -35,6 +71,7 @@ int getPosition()
 	return 1;
 }
 
+//sets the motors to a certain left and right value
 void setMotors(int left, int right)
 {
 	motor[motorFL] = left;
@@ -43,6 +80,7 @@ void setMotors(int left, int right)
 	motor[motorBR] = right;
 }
 
+//stops all motors from running
 void stopMotors()
 {
 	motor[motorFL] = 0;
@@ -51,75 +89,113 @@ void stopMotors()
 	motor[motorBR] = 0;
 }
 
-void move(int dir, int encoderValue, int power, int time = 5000) //encoders go up to 280 with the new motors = 1 rotation
+//our way to move forward and backwards while allowing the gyro to adjust the robot if needed
+void move(int encoderValue, int power, int time = 5000)
 {
-	float heading = 0;
-	float trueHeading = 0; //the direction the robot is really facing, in case the gyro screws up
-	HTGYROstartCal(gyroSensor); //heading 180 -180 while() L- R+
+	float heading = 0; //what our gyro will read
+	float trueHeading = 0; //what it truly is in our case
+	HTGYROstartCal(gyroSensor); //calibrates the gyro to set its current position to "default"
 
-	wait1Msec(500);
-	clearTimer(T1);
-	if(dir == 1) //if direction is 1, go forward
+	wait1Msec(500); //wait for the gyro to complete its calibration
+	clearTimer(T1); //clear the nxt timer so we can use it
+
+	if(encoderValue > 0) //if the encoder value is positive, we are going forward
 	{
 		nMotorEncoder[motorFR] = 0; //motor encoder reset to 0 before going forward
 		nMotorEncoder[motorFL] = 0; //motor encoder reset to 0 before going forward
+		//this is where the magic happens, while we are not reaching our distance yet and we still have time
 		while((getEncoderAverage(nMotorEncoder[motorFR], nMotorEncoder[motorFL]) < encoderValue) && (time1[T1] < time))
+		{
+			heading = HTGYROreadRot(gyroSensor); //get the current rotation degree
+			trueHeading += heading * (20/1000); //converts the current rotation value to the true degree
+
+			//if the robot is not +1/-1 its forward heading
+			if(!gyroThreshhold(trueHeading, 0, 1))
+			{
+				if(trueHeading > 0) //if the robot drifted to the right
+				{
+					setMotors(power/4, power); //sets the motors to adjust
+				}
+
+				if(trueHeading < 0) //if the robot drifted to the left
+				{
+					setMotors(power, power/4); //sets the motors to adjust
+				}
+			}
+			else //if the robot did not drift
+			{
+				setMotors(power, power); //sets the motors to simply go forward
+			}
+			wait1Msec(20); // gyro can check its rotation value every 20 ms
+		}
+	}
+
+	if(encoderValue < 0)
+	{
+		nMotorEncoder[motorFR] = 0; //motor encoder reset to 0 before going forward
+		nMotorEncoder[motorFL] = 0; //motor encoder reset to 0 before going forward
+		while((getEncoderAverage(nMotorEncoder[motorFR], nMotorEncoder[motorFL]) > encoderValue) && (time1[T1] < time))
 		{
 			heading = HTGYROreadRot(gyroSensor); //get the current rotation degree
 			trueHeading += heading * (20/1000); //converts the current rotation value to the true degrees
 
 			if(!gyroThreshhold(trueHeading, 0, 1))
 			{
-				if(trueHeading > 0) //if the robot drifted to the right
+				if(trueHeading > 0) //if the robot drifted to the left
 				{
-					setMotors(power/4, power);
+					setMotors(-power, -power/4);
 				}
 
-				if(trueHeading < 0) //if the robot drifted to the left
+				if(trueHeading < 0) //if the robot drifted to the right
 				{
-					setMotors(power, power/4);
+					setMotors(-power/4, -power);
 				}
 			}
 			else //if the robot did not drift
 			{
-				setMotors(power, power);
+				setMotors(-power, -power);
 			}
 			wait1Msec(20); // gyro can check its rotation value every 20 ms
 		}
 	}
+
 	stopMotors();
 }
 
-void turn(int power, int deg, int dir, int time = 5000)
+void turn(int power, int deg, int time = 5000)
 {
 	float heading = 0;
 	float trueHeading = 0;
 
 	HTGYROstartCal(gyroSensor);
+
 	wait1Msec(500);
 	clearTimer(T1);
 	//turning right
-	if(dir == 1)
+	if(deg > 0)
 	{
-		while(!gyroThreshhold(trueHeading, deg, 1) && (time1[T1] < time))
+		while(!gyroThreshRot(trueHeading, deg, 1) && (time1[T1] < time))
 		{
-			setMotors(power, 0);
+			setMotors(power, 0); //left and right
 			heading = HTGYROreadRot(gyroSensor); //get the current rotation degree
 			trueHeading += heading * (20/1000); //converts the current rotation value to the true degrees
 			wait1Msec(20);
 		}
+		stopMotors();
 	}
 	//turning left
-	if(dir == 2)
+	if(deg < 0)
 	{
-		while(!gyroThreshhold(trueHeading, deg, 1) && (time1[T1] < time))
+		while(!gyroThreshRot(trueHeading, deg, 1) && (time1[T1] < time))
 		{
-			setMotors(0, power);
+			setMotors(0, power); //left and right
 			heading = HTGYROreadRot(gyroSensor); //get the current rotation degree
 			trueHeading += heading * (20/1000); //converts the current rotation value to the true degrees
 			wait1Msec(20);
 		}
+		stopMotors();
 	}
+	stopMotors();
 }
 
 void moveOffRamp(int deg, int power, int time = 3000)
@@ -147,4 +223,29 @@ void lifter(int deg, int time = 5000)
 		motor[motorLift] = 100;
 	}
 	motor[motorLift] = 0;
+	servo[pivotServo] = 0;
+	servo[latchServo] = 126;
+	wait1Msec(500);
+	servo[latchServo] = 64;
+	servo[pivotServo] = 160;
+	while((nMotorEncoder[motorLift] > 0) && time1[T1] < time)
+	{
+		motor[motorLift] = -100;
+	}
+	motor[motorLift] = 0;
+}
+
+void grabber(bool x)
+{
+	//true, AKA Grab
+	if(x)
+	{
+			servo[grabberRight] = 255;
+			servo[grabberLeft] = 0;
+	}
+	else
+	{
+			servo[grabberRight] = 140;
+			servo[grabberLeft] = 120;
+	}
 }

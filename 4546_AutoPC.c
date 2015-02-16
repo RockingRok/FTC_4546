318dbd543ba4abf09966d70fedd1b050c11db9a5
ruvmu <ruvmu1@gmail.com>
2015-02-16 17:05:18 -0600

    Park Center complete
    1&3- Very Stable 2-Kind of stable

--- 4546_AutoPC.c
+++ 4546_AutoPC.c
@@ -24,40 +24,37 @@
 #include "JoystickDriver.c"
 task main()
 {
-	//waitForStart();
+	waitForStart();
 	clearTimer(T2);
 	initialize();
 	//come up to the last intersection before the center goal
 	move(40, 1350, 3000);
-	wait1Msec(500);
+	wait1Msec(1000);
 	if(getPositionPark() == 1)
 	{
-		centerGoal(100, 180); //183
+		centerGoal(100, 203); //183
 		//while(time1[T2] < 25000){}
 		rotTurn(100, 90);
-		move(-50, -700, 1500);
+		move(-50, -600, 1500);
 		rotTurn(100, -90);
 		move(-60, -2000, 3000);
 	}
 	else if(getPositionPark() == 2)
 	{
-		move(-50, -200, 1000);
-		rotTurn(100, -45); //-39
-		wait1Msec(2000);
-		move(50, 1100, 3000); //1200
-		//rotTurn(100, -90); //-83
-		//move(50, 100, 1000);
+		move(-30, -500, 1000);
+		rotTurn(100, -45);
+		move(30, 1670, 3000);
 		centerGoal(100, -90);
 		//while(time1[T2] < 25000){}
 		rotTurn(100, 90);
-		move(-50, -700, 1500);
+		move(-50, -550, 1500);
 		rotTurn(100, -90);
 		move(-60, -2000, 3000);
 	}
 	else if(getPositionPark() == 3)
 	{
-		rotTurn(100, -45);
-		move(50, 1836, 3000); //2000
+		rotTurn(100, -40);
+		move(30, 2400, 3000); //2000
 		centerGoal(100, -45);
 		//while(time1[T2] < 25000){}
 		rotTurn(100, 90);
